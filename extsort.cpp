a2e6c7c4a931b11ab67fc7e7b0ea5b8f424e0f6d
MaximRussia <opengl1@yandex.ru>
2016-01-03 23:26:38 +0300

    refactoring

--- extsort.cpp
+++ extsort.cpp
@@ -18,62 +18,69 @@ using namespace std;
 #define _p(v) { cout << v << endl; }
 
 #define BUF_SIZE ( 25 * 1024 * 1024 ) // 25MB
-#define LINE_MAX_SIZE ( 1024 )
+#define LINE_MAX_SIZE ( 1024 )        // 1024 chars per line
 #define MAX_READ_COUT ( BUF_SIZE / LINE_MAX_SIZE )
 
-struct chunk
-{
-	chunk( FILE* f ) : f_( f ) { line_.resize(LINE_MAX_SIZE); }
-	~chunk() {}
+struct chunk {
+	chunk( FILE* f ) : f( f ) {
+	    line = new char[LINE_MAX_SIZE];
+    }
+	~chunk() {} // { delete []line; fclose(f); }
 
-	bool pop_line( void )
-	{
-        	return fgets( const_cast<char*>(line_.c_str()) /* OH! GUSH! SO NAUGHTY! */, LINE_MAX_SIZE, f_ );
-    	}
+	bool pop_line() {
+        return fgets(line, LINE_MAX_SIZE, f);
+    }
 
-	FILE*	f_;
-	string	line_;
+	FILE*	f;
+	char*	line;
 };
 
-int main( int argc, char** argv )
-{
+int main( int argc, char** argv ) {
+	FILE*	 in	= NULL;
+	FILE*    out = NULL;
 
-	assert(MAX_READ_COUT > 0); // so kek
+	if(argc > 2) {
+        in = fopen( argv[ 1 ], "rb" );
+        out = fopen( argv[ 2 ], "wt" );
+	}
+	else {
+        in = fopen( "big.txt", "rb" );
+        out = fopen( "out.txt", "wt" );
+	}
 
-	FILE*	 in	= fopen( argv[ 1 ], "rb" );
-	char*	 line	= (char*) malloc( LINE_MAX_SIZE ); // memory buffer
+	char*	 line	= new char[LINE_MAX_SIZE]; // memory buffer
 	unsigned read_count = MAX_READ_COUT;
 
 	vector<chunk>	chunks;
 	vector<string>	lines;
 
-	auto add_chunk = [&]()
-	{
-		//
-        	// sort lines and write in chunk file
-        	//
+	auto add_chunk = [&]() {
+        //
+        // sort lines and write in chunk file
+        //
 
 		std::sort( lines.begin(), lines.end(), [&]( const string& a, const string& b ) { return a < b; } );
 
 		string fname = "sort";
-		fname += to_string(chunks.size());
+		fname += std::string(1, '0' + chunks.size());
 
 		_p("add chunk " + fname)
 
-		FILE* out = fopen( fname.c_str(), "wb+" );
-		chunks.push_back( out );
+		FILE* _out = fopen( fname.c_str(), "wb+" );
 
 		for( auto& s : lines )
-			fwrite( s.c_str(), strlen( s.c_str() ), 1, out );
+			fwrite( s.c_str(), strlen( s.c_str() ), 1, _out );
 
-		fseek( out, 0, SEEK_SET );
+		fseek( _out, 0, SEEK_SET );
+
+		chunks.push_back( _out );
+		chunks.back().pop_line();
 
 		//clear lines
 		lines.clear();
 	};
 
-	while( fgets( line, LINE_MAX_SIZE, in ) )
-	{
+	while( fgets( line, LINE_MAX_SIZE, in ) ) {
 		lines.push_back( string(line) );
 		read_count--;
 
@@ -81,29 +88,25 @@ int main( int argc, char** argv )
 		{
 			add_chunk();
 			read_count = MAX_READ_COUT;
-        	}
+        }
 	}
 
 	// if after 'while' lines not empty, create one more chunk
 	if( !lines.empty() ) add_chunk();
 
-	_p("use " + to_string(chunks.size()) + " chunks");
+	_p("use " << chunks.size() << " chunks");
 
-	FILE* out = fopen( argv[ 2 ], "wt" );
-	for( auto& c : chunks ) c.pop_line(); // read line from chunks
-
-	while( chunks.size() )
-	{
-		std::sort( chunks.begin(), chunks.end(), [&]( const chunk& a, const chunk& b ) { return a.line_ < b.line_; } );
-		fwrite( chunks.back().line_.c_str(), strlen( chunks.back().line_.c_str() ), 1, out );
-
-		if( !chunks.back().pop_line() )
-			chunks.pop_back();
+	while( !chunks.empty() ) {
+		std::sort( chunks.begin(), chunks.end(), [&]( const chunk& a, const chunk& b ) { return strcmp(a.line, b.line) < 0; } );
+		fwrite( chunks.back().line, strlen( chunks.back().line ) - 1, 1, out );
+		if( !chunks.back().pop_line() ) chunks.pop_back();
 	}
 
 	_p("done");
- 
+
 	fclose( out );
-	fclose ( in );
+	fclose( in );
 	delete []line;
+	chunks.clear();
+	lines.clear();
 }
