d26968fc28b717d4176c988e6aaca5ffe7ed5150
MaksimRussia <opengl88@gmail.com>
2016-10-11 12:45:06 +0300

    update

--- extsort.cpp
+++ extsort.cpp
@@ -1,16 +1,9 @@
-/*
-    base64 /dev/urandom | head -c 1000000 > big.txt
-    g++ -std=c++11 extsort.cpp && time ./a.out big.txt out.txt
-    du big.txt out.txt
-    wc big.txt out.txt
-    wc out.txt big.txt && du out.txt big.txt
-    g++ -std=c++11 extsort.cpp && time ./a.out big.txt out.txt && wc out.txt big.txt && du out.txt big.txt
-*/
 
 #include <iostream>
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <iterator>
 #include <map>
 #include <set>
 #include <queue>
@@ -25,145 +18,161 @@
 #include <memory>
 using namespace std;
 
-const size_t BUF_SIZE = 1024 * 1024; // 1MB
-const size_t LINE_MAX_SIZE = 100;
-const size_t MAX_READ_COUT = ( BUF_SIZE / LINE_MAX_SIZE );
+typedef unsigned long long int _uint64;
+
+_uint64 LINE_MAX_SIZE = 256;
 
 struct chunk {
-	chunk( FILE* f ) : f( f ) {
+	chunk(FILE* f) :f (f) {
 	    line = new char[LINE_MAX_SIZE];
-    }
-	~chunk(){}; // { delete []line; fclose(f); }
+	}
+	~chunk() {};
 
-	/*
-	chunk(const chunk& ch) {
-	    f = ch.f;
-	    line = ch.line;
+	void fini() {
+		clean();
+		fclose(f);
 	}
 
-	chunk(const chunk&& ch) {
-	    f = ch.f;
-	    line = ch.line;
+	void clean() {
+		delete[]line;
 	}
-    */
 
 	bool pop_line() {
-        return fgets(line, LINE_MAX_SIZE, f);
-    }
+		return fgets(line, LINE_MAX_SIZE, f) != NULL;
+	}
 
 	FILE*	f;
 	char*	line;
 };
 
-FILE*	 in	= NULL;
+FILE*	 in = NULL;
 FILE*    out = NULL;
-
-char*	 line	= new char[LINE_MAX_SIZE]; // memory buffer
-unsigned read_count = MAX_READ_COUT;
+char*    line = NULL;
 
 vector<chunk>	chunks;
 vector<string>	lines;
+vector<string>  files;
 
 string next() {
-    static size_t num = 0;
-    stringstream ss;
-    ss << num++;
-    return ss.str();
+	static _uint64 num = 0;
+	stringstream ss;
+	ss << num++;
+	return ss.str();
 }
 
-bool cmp_1( const string& a, const string& b ) { return a > b; }
-bool cmp_2( const chunk& a, const chunk& b ) { return strcmp(a.line, b.line) < 0; }
+bool cmp_1(const string& a, const string& b) { return a < b; }
+bool cmp_2(const chunk& a, const chunk& b) { return strcmp(a.line, b.line) > 0; }
 
-void add_chunk () {
-    //
-    // sort lines and write in chunk file
-    //
-
-    std::sort( lines.begin(), lines.end(), cmp_1 );
-
-    string fname = "sort_" + next();
-    FILE* _out = fopen( fname.c_str(), "wb+" );
-
-    for( int i = 0; i < lines.size(); i++ )
+void write_chunk(FILE* _out) {
+	sort(lines.begin(), lines.end(), cmp_1);
+    for( _uint64 i = 0; i < lines.size(); ++i ) {
         fwrite( lines[i].c_str(), strlen(lines[i].c_str()), 1, _out );
-
-    fseek( _out, 0, SEEK_SET );
-
-    chunks.push_back( _out );
-    chunks.back().pop_line();
-
-    //clear lines
+    }
     lines.clear();
+}
+
+void add_chunk() {
+	string fname = "sort_" + next();
+	FILE* _out = fopen(fname.c_str(), "wb+");
+	write_chunk(_out);
+	fseek( _out, 0, SEEK_SET );
+	chunks.push_back(chunk(_out));
+	files.push_back(fname);
+	chunks.back().pop_line();
 };
 
 void complete() {
-	fclose( out );
-	fclose( in );
-	delete []line;
+	fclose(out);
+	fclose(in);
+	delete[] line;
+	for( _uint64 i = 0; i < chunks.size(); ++i ) {
+		chunks[i].fini();
+	}
+	for( _uint64 i = 0; i < files.size(); ++i ) {
+		remove(files[i].c_str());
+	}
+	files.clear();
 	chunks.clear();
 	lines.clear();
 }
 
+_uint64 fize_size(char* s) {
+    return ifstream(s, std::ifstream::ate | std::ifstream::binary).tellg();
+}
+
 int main(int argc, char* argv[]) {
 
-    try {
-        if(BUF_SIZE <= 0) {
-            throw std::runtime_error("allocate 0 or smaller");
-        }
+	try {
 
-        size_t fileSize = ifstream("big.txt", std::ifstream::ate | std::ifstream::binary).tellg();
-        if(!ifstream("big.txt").good() || !fileSize) {
-            throw std::runtime_error("input file error");
-        }
+		if (argc != 4) {
+			throw ("invalid args");
+		}
 
-        ////////////////////////////
-        char *t = new char[BUF_SIZE];
-        delete[] t;
-        ////////////////////////////
+		_uint64 BUF_SIZE = atoll(argv[3]);
 
-        in = fopen( "big.txt", "rb" );
-        out = fopen( "out.txt", "wt" );
+		// check allocation
+		char* buf = new char[BUF_SIZE];
+		delete[]buf;
 
-        if(fileSize <= BUF_SIZE) { // sort in memory, lucky!
-            while( fgets( line, LINE_MAX_SIZE, in ) ) {
-                lines.push_back( string(line) );
-            }
-            std::sort( lines.begin(), lines.end(), cmp_1 );
+		_uint64 MAX_READ_COUT = ( BUF_SIZE / LINE_MAX_SIZE ) + 1;
+		_uint64 read_count = MAX_READ_COUT;
 
-            while( !lines.empty() ) {
-                fwrite( lines.back().c_str(),
-                       strlen(lines.back().c_str())-1, 1, out );
-                lines.pop_back();
-            }
+		in = fopen(argv[1], "rb");
 
-            complete();
-            return 0;
-        }
+		if (!in) {
+			throw ("input file error");
+		}
 
-        while( fgets( line, LINE_MAX_SIZE, in ) ) {
-            lines.push_back( string(line) );
-            read_count--;
+		_uint64 fileSize = fize_size(argv[1]);
+		if (!fileSize) {
+			throw ("empty file error");
+		}
 
-            if( read_count <= 0) {
-                add_chunk();
-                read_count = MAX_READ_COUT;
-            }
-        }
+		out = fopen(argv[2], "wb");
+		line = new char[LINE_MAX_SIZE];
 
-        // if after 'while' lines not empty, create one more chunk
-        if( !lines.empty() ) add_chunk();
+		if (fileSize <= BUF_SIZE) { // sort in memory, lucky!
+			while (fgets(line, LINE_MAX_SIZE, in) != NULL) {
+				lines.push_back(string(line));
+			}
+			write_chunk(out);
+			complete();
+			return 0;
+		}
 
-        while( !chunks.empty() ) {
-            std::sort( chunks.begin(), chunks.end(), cmp_2);
-            fwrite( chunks.back().line, strlen(chunks.back().line)-1, 1, out );
-            if( !chunks.back().pop_line() ) chunks.pop_back();
-        }
+		while (fgets(line, LINE_MAX_SIZE, in) != NULL) {
+			lines.push_back(string(line));
+			read_count--;
+			if (read_count <= 0) {
+				add_chunk();
+				read_count = MAX_READ_COUT;
+			}
+		}
 
-        complete();
-    } catch (std::exception &ex) {
-        cout << "exception : " << ex.what() <<  endl;
-        complete();
-    }
+		if (!lines.empty()) {
+            add_chunk();
+		}
 
-    return 0;
+		while (!chunks.empty()) {
+			sort(chunks.begin(), chunks.end(), cmp_2);
+			fwrite(chunks.back().line, strlen(chunks.back().line), 1, out);
+			if (!chunks.back().pop_line()) {
+				chunks.back().fini();
+                chunks.pop_back();
+			}
+		}
+
+		complete();
+	} catch (const char *ex) {
+		cout << "RUNTIME    : " << ex << endl;
+		complete();
+	} catch (bad_alloc &ex) {
+		cout << "ALLOCATOR  : " << ex.what() << endl;
+		complete();
+	} catch (exception &ex) {
+		cout << "COMMON     : " << ex.what() << endl;
+		complete();
+	} 
+
+	return 0;
 }
---
+++ test.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+echo "testing ... "
+g++ -std=c++03 extsort.cpp
+
+echo "invalid args"
+./a.exe 
+
+echo "invalid args"
+./a.exe empty.txt out.txt 100000000 hello test test
+
+echo "invalid args"
+./a.exe empty.txt out.txt hello test test
+
+echo "invalid args"
+./a.exe empty.txt out.txt hello 
+
+echo "invalid buffer size"
+./a.exe empty.txt out.txt -1024
+
+echo "invalid buffer size"
+./a.exe empty.txt out.txt 25000000000
+
+echo "invalid args"
+./a.exe non.valid.input.txt out.txt 100000000 
+
+echo "empty ext"
+./a.exe empty.txt out.txt 100000000
+
+echo "empty in memory"
+./a.exe empty.txt out.txt 100000000
+echo "----------------"
+
+custom_testing() {
+	echo "$1"
+	time ./a.exe "$2" "$3" "$4"
+	if [[ $(diff <(sort "$2") <(cat "$3") | head) ]]; then
+		echo "TEST FAILED !!!"
+		diff <(sort "$2") <(cat "$3")
+	else
+		echo "TEST OK"
+	fi 
+	echo "----------------"
+}
+
+custom_testing "normal ext" normal.txt out.normal.ext.txt 6000000
+custom_testing "normal in memory" normal.txt out.normal.in.txt 100000000
+custom_testing "numbers ext" numbers.txt out.numbers.ext.txt 2000 
+custom_testing "numbers in memory" numbers.txt out.numbers.in.txt 100000000 
+custom_testing "nonascii ext" nonascii.txt out.nonascii.ext.txt 2000 
+custom_testing "nonascii in memory" nonascii.txt out.nonascii.in.txt 100000000 
+
+stress_testing() {
+	base64 /dev/urandom | head -c "$1" > big.txt
+	time ./a.exe big.txt out.big.ext.txt "$2"
+	head out.big.ext.txt
+	tail out.big.ext.txt
+	rm out.*.txt big.txt
+	echo "----------------"
+}
+
+#echo "stress testing... "
+#stress_testing 1000000000 300000000 
+#stress_testing 2000000000 600000000 
+#stress_testing 5000000000 1000000000
+#stress_testing 10000000000 1000000000 
